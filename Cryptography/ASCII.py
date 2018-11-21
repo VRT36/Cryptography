6996e6489012cb08ab1fb9735f5c40638028aa2a
Vasanth R <39703004+VRT36@users.noreply.github.com>
2018-11-21 18:55:57 +0530

    Update ASCII.py

--- Cryptography/ASCII.py
+++ Cryptography/ASCII.py
@@ -1,15 +1,15 @@
 def encrypt():
     raw = raw_input('enter the text\n')
-    out = ''
-    for i in raw :
-        out += ord(i)
+    out = []
+    for i in raw:
+        out.append(ord(i)) 
     print 'encrypted text==>',out
 
 def decrypt():
-    raw = raw_input('enter the text\n')
-    out = ''
+    raw = raw_input('enter the text( no comma )\n')
+    out = []
     for i in raw :
-        out += chr(i)
+        out.append(chr(i))
     print 'decrypted text==>',out
 
 print('========ASCII========\n\n')
@@ -25,8 +25,8 @@ else:
 
 '''
 Created by: Vasanth R
-Time      : 21:37
-Data      : 20/11/2018
+Time      : 18:55
+Data      : 21/11/2018
 '''
 
     
