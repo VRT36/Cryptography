d5cb6c8aa784c094ab5d878ccc023b493e7c4c95
Vasanth R <39703004+VRT36@users.noreply.github.com>
2018-12-14 19:29:22 +0530

    Create hex-text.py

---
+++ tools/HEX/hex-text.py
@@ -0,0 +1,14 @@
+def decrypt():
+    raw = raw_input('enter the text\n')
+    raw = raw.decode('hex')
+    print 'decrypted text==>',raw
+
+print('========Hexadecimal========\n\n')
+
+decrypt()
+
+'''
+Created by: Vasanth R
+Time      : 21:37
+Data      : 20/11/2018
+'''
